5590b9e3a3a80773d57f095f9b037499e21a20ad
R. Scott Shipley <106488991+RScottShipley@users.noreply.github.com>
2022-08-15 18:05:38 -0700

    Finished product of the project

---
+++ src/util/Yelp.js
@@ -0,0 +1,33 @@
+const apiKey = 'Fc_maoa8Hq3BqdJq5T_Alq-Yp-E62epL9GmWE3gnBgcZVW44uUgZuV9TqzWaHIDC9QKJfvcs7DKBa7w67vTBA3Dn1i4mhRVxbPJVuFXf6e-NKZlZO3qZmjqNo9z6YnYx';
+
+export const Yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers : {
+                Authorization : `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            console.log(jsonResponse);
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business["name"],
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    };
+                });
+            }
+        });
+    }
+};
+
+export default Yelp;
